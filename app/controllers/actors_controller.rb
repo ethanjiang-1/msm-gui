2cd6f91b9092b7cad47763c6ab9cbd5f4ec01090
ethanjiang-1 <146405629+ethanjiang-1@users.noreply.github.com>
2025-05-06 16:14:40 +0000

    finished

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,42 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    @the_actor = Actor.new
+    @the_actor.name = params.fetch("query_name")
+    @the_actor.dob = params.fetch("query_dob")
+    @the_actor.bio = params.fetch("query_bio")
+    @the_actor.image = params.fetch("query_image_url")
+
+    @the_actor.save
+    redirect_to("/actors")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_actors = Actor.where({ :id => the_id })
+    @the_actor = matching_actors.at(0)
+
+    @the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+
+    matching_actors = Actor.where({ :id => the_id })
+    @the_actor = matching_actors.at(0)
+
+    @the_actor.name = params.fetch("query_name")
+    @the_actor.dob = params.fetch("query_dob")
+    @the_actor.bio = params.fetch("query_bio")
+    @the_actor.image = params.fetch("query_image_url")
+
+    @the_actor.save
+
+    redirect_to("/actors/#{@the_actor.id}")
+  end
 end
